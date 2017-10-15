b5bae974964569ae010c80e5833a951d17c6fb1e
Stefanos Xanthopoulos <stefanosx@gmail.com>
2017-10-15 23:55:10 +0200

    Return auth token on user creating

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,13 +3,9 @@ class UserController < ApplicationController
   def create
     user = User.create(full_name: params[:full_name], email: params[:email], password: params[:password])
     if user.id.present?
-      render json: {message: "User created"}
+      render json: {message: "User created", auth_token: user.authentication_token}
     else
       render json: {message: "User did not created", error: {messages: user.errors.full_messages}}, status: 422
     end
-  end
-
-  def index
-    render json: {status: "hey"}
   end
 end
